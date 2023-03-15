ee9c9ae4869bad19f9bdad4209edb065af9660b0
Artur Cavalcante <artursousac@hotmail.com>
2023-03-15 00:56:14 -0300

    Mentoria Banco PAN acerca de Piramides de Testes, TDD e BDD

---
+++ AulasDIO/src/mentoriasDIO/piramideTestesTDD/Main.java
@@ -0,0 +1,74 @@
+package mentoriasDIO.piramideTestesTDD;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/*
+		 * Teste de software valida um sistema/aplicação desde a etapa
+		 * de criação até a sua implementação.
+		 * Os testes devem cobrir o máximo de combinações que podem ser aplicados
+		 * no software.
+		 * Abertura de bugs e novo teste após a correção.
+		 * 
+		 * Pontos positivos para isto: Encontra os bugs na fase de desenvolvimento,
+		 * garante a entrega com maior qualidade, além de verificar se todos os requisitos
+		 * foram implementados. Também, reduz os custos de manutenções e correções de erros em produção.
+		 * 
+		 * -> Tipos de teste: - Teste de Unidade (Teste Unitário); - Teste de Integração
+		 * - Teste Operacional; - Teste de Regressão; - Teste Funcional; - Teste de Interface
+		 * - Testes de Configuração e Segurança; - Teste de Performance; - Teste de Carga
+		 * - Testes de Stress; - Testes de Instalação/Desinstalação
+		 * 
+		 * -> TDD (Test Driven Development ou Desenvolvimento Orientado por Testes):
+		 * Desenvolvimento orientado a testes. Ciclo de repetição: Teste falha > Implementação
+		 * da funcionalidade é realizado > Teste Ok.
+		 * Refatoramento do código com boas práticas.
+		 * 
+		 * Pontos positivos: - Maior cobertura de testes unitários; - Testes unitários
+		 * executados com maior frequência; - Códigos mais limpos; - Maior segurança na correção de bugs
+		 * - Reforço à cultura da qualidade e às práticas de shift-left.
+		 * 
+		 * -> BDD (Behavior Driven Development ou Desenvolvimento Orientado ao Comportamento):
+		 * Desenvolvimento orientado ao comportamento e é um complemento do TDD.
+		 * Metodologia compartilhada entre o QA - Dev - PO (regra dos 3 amigos).
+		 * Given (Dado) - When (Quando) - Then (Então).
+		 * 
+		 * Pontos Positivos: - Produto que responde a expectativa do cliente;
+		 * - Melhor comunicação e integração do time; - Documentação dinâmica.
+		 * 
+		 * -> BDD x TDD
+		 * TDD pensa na qualidade do código e o BDD pensa a partir do ponto de vista
+		 * do comportamento esperado de uma funcionalidade pelo usuário.
+		 * É possível aplicar as duas metodologias no mesmo desenvolvimento.
+		 * 
+		 * -> Pirâmide de Testes:
+		 * Base: Testes de Unidade; Meio: Testes de Integração; Topo: Testes Ponta a Ponta
+		 * (E2E, UI ou Testes de Interface).
+		 * 
+		 * Teste de Unidade: Testes realizados na menor parte testável de uma aplicação
+		 * Eles tendem a ser extremamente pequenos e de rápida criação/execução.
+		 * Utilizados na metodologia TDD. As boas práticas sugerem que desenvolvedores
+		 * sejam os responsáveis pela criação de testes unitários.
+		 * 
+		 * Teste de Integração: Têm como objetivo testar um conjunto de unidades interagindo
+		 * entre si, como, por exemplo, banco de dados, comunicação de interfaces, APIs, micro-serviços.
+		 * Por testar um conjunto de unidades, estes testes acabam sendo mais complexos de fazer
+		 * do que um teste de unidade.
+		 * Necessita um ambiente completo para sua execução.
+		 * 
+		 * Teste Ponta a Ponta: Tem por objetivo principal simular o comportamento de um usuário final
+		 * em nossa aplicação. É bastante utilizado na metodologia BDD.
+		 * É necessário uma escolha criteriosa na cobertura dos cenários, uma vez que são testes
+		 * mais complexos.
+		 * 
+		 * -> Certificações iniciais de testes: - BSTQB/ISTQB (CTFL e CTFL-AT); 
+		 * - ALATS (CBTS)
+		 * 
+		 * 
+		 * 
+		 * 
+		 */
+
+	}
+
+}
