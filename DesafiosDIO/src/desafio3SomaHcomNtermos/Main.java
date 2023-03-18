f557f9b20b96aab9059d96a73310cf1b78712d67
Artur Cavalcante <artursousac@hotmail.com>
2023-03-18 02:30:41 -0300

    Resolve o DESAFIO 03

--- DesafiosDIO/src/desafio3SomaHcomNtermos/Main.java
+++ DesafiosDIO/src/desafio3SomaHcomNtermos/Main.java
@@ -1,25 +1,24 @@
 package desafio3SomaHcomNtermos;
 
-/*
- * Neste desafio, faça um programa que calcule o valor de H com N termos. 
- * Sendo, H = 1 + 1/2 + 1/3 + 1/4 + ... + 1/N. 
- */
-
 import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] Args) {
+	public static void main(String[] args) {
 
 		double h = 0;
 		Scanner sc = new Scanner(System.in);
 		double n = sc.nextDouble();
 
 		for (int i = 1; i <= n; i++) {
-        
+			h = h + (1.0/i);
 		}
+		
+		
+		System.out.println((int)Math.round(h));
+		
+		sc.close();
 
-    
 	}
-	
+
 }
