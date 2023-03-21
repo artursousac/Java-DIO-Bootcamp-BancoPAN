6b9165a9a09b37cdd6393d7169feaf8df4980c2e
Artur Cavalcante <artursousac@hotmail.com>
2023-03-20 21:56:25 -0300

    Inicio do modulo de Collections Java

---
+++ AulasDIO/src/collectionsJava/ConceitosCollections.java
@@ -0,0 +1,23 @@
+package collectionsJava;
+
+public class ConceitosCollections {
+	
+	/*
+	 * Collection é um objeto que agrupa múltiplos elementos (variáveis primitivas ou objetos)
+	 * dentro de uma única unidade.
+	 * Serve para armazenar e processar conjuntos de dados de forma eficiente.
+	 * Collections é composta por Interfaces, Implementações ou Classes, Algoritmos.
+	 * 
+	 * Interface List com as implementações ArrayList e LinkedList.
+	 * Interface Set com as implementações HashSet e LinkedHashSet.
+	 * Interface Map (Não estende a Interface Collection, mas está dentro do universo Collection)
+	 * com as implementações HashMap e LinkedHashMap
+	 * TreeSet faz parte da Set e TreeMap da Map, mas relacionado a árvores.
+	 * 
+	 * Classe Collections e Arrays;
+	 * 
+	 * 
+	 * 
+	 */
+
+}
