dcdebbb590b85240df0808531b881c80df0a9882
Artur Cavalcante <artursousac@hotmail.com>
2023-04-10 12:05:06 -0300

    Inicio do modulo de springs

---
+++ SpringFramework/src/springFramework/Introducao.java
@@ -0,0 +1,40 @@
+package springFramework;
+
+public class Introducao {
+
+	public static void main(String[] args) {
+		/*
+		 * Spring possui módulos, como o Data Access/Integration (JDBC,
+		 * ORM, OXM, JMS, Transactions), o Web (Web, Servlet, Portlet,
+		 * Struts), o AOP, o Core Container (Beans, Core, Context, Expression
+		 * Language) e o de Testes.
+		 * 
+		 * Spring foi um movimento por conta da Java EE.
+		 * Java EE era complicado e o Spring era um caminho mais simples e 
+		 * fácil de evoluir.
+		 * 
+		 * Inversão de Controle é o redirecionamento do fluxo de execução
+		 * de um código, retirando parcialmente o controle sobre ele e delegando
+		 * para um container.
+		 * O principal motivo é minimzar o acoplamento do código.
+		 * 
+		 * Injeção de Dependências é um padrão de desenvolvimento com a finalidade
+		 * de manter baixo o nível de acoplamento entre módulos de um sistema.
+		 * 
+		 * Beans é um objeto que é instaciado (criado), montado e gerenciado por um
+		 * container por meio do princípio da inversão de controle.
+		 * 
+		 * Scopes existem 5 tipos, como o Singleton e Prototype (Padrões) e 
+		 * os HTTP (Request, Session e Global).
+		 * Singleton: Define apenas uma instância do objeto.
+		 * Prototype: Vai ser criado um novo objeto a cada solicitação do container.
+		 * Request: Bean criado para cada requisição HTTP. Existirão enquanto a requisição estiver em execução.
+		 * Session: Bean criado para cada sessão de usuário.
+		 * Global: Cria um bean para o ciclo de vida do contexto da aplicação.
+		 * Autowired: uma anotação onde deve ocorrer uma injeção automática de dependencia.
+		 * 
+		 */
+
+	}
+
+}
---
+++ SpringFramework/src/springFramework/springBoot/Introducao.java
@@ -0,0 +1,31 @@
+package springFramework.springBoot;
+
+public class Introducao {
+
+	public static void main(String[] args) {
+		/*
+		 * Spring Framework é baseado no padrão de injeção de dependências,
+		 * enquanto o Springboot foca na configuração automática.
+		 * 
+		 * Antes do boot, os desafios eram as dependências individuais, a verbosidade
+		 * a incompatibilidade de versões, a complexidade de gestão, além da
+		 * configuração complexa e repetitiva.
+		 * 
+		 * Dado que a maior parte das configurações necessárias para o inicio de um projeto
+		 * são sempre as mesmas, ele inicia um projeto com todas as configurações necessárias
+		 * já incluídas.
+		 * 
+		 * Starters: Descritores de dependência.
+		 * Benefícios de Starters é a Coesão, Versões Compatíveis, Otimização do tempo,
+		 * Configuração simples e Foco no negócio.
+		 * Alguns Starters: data-jpa (integração ao banco de dados via JPA - Hibernate.), data-mongodb (integração
+		 * com banco de dados MongoDB), web (inclusão de container Tomcat para aplicações REST),
+		 * web-services: Webservices baseados na arquitetura SOAP.
+		 * batch, test, openfeign e actuator são alguns outros.
+		 * 
+		 * 
+		 */
+
+	}
+
+}
